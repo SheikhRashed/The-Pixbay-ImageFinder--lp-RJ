abf21bc47a96e2c2aa06a1a26d3be1009c1ff5a2
SheikhRashed <devrashed06@gmail.com>
2022-08-12 13:47:53 +0600

    update: all hooks included

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react"
-import Header from "./Components/Header/Header";
+// import Header from "./Components/Header/Header";
+import { Container, Navbar, Form } from "react-bootstrap"
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import ImageLists from "./Components/ImageLists/ImageLists";
-function App() {
 
+import styles from "./Components/Header/Header.module.css"
+
+function App() {
+  const [search, setSearch] = useState("")
+  const [limit, setLimit] = useState(5)
   const [pictures, setPictures] = useState([])
-  async function fetchPicture() {
+
+  console.log(search)
+
+  const searchHandler = (e) => setSearch(e.target.value)
+  const showPerPage = (e) => setLimit(e.target.value)
+
+
+
+  async function fetchPicture(search, limit) {
     try {
-      fetch(`https://pixabay.com/api/?key=29176684-20b5605bcebb5041e4d8821ca&q=flower&image_type=photo&per_page=10&safesearch=true`)
+      fetch(`https://pixabay.com/api/?key=29176684-20b5605bcebb5041e4d8821ca&q=${search}&image_type=photo&per_page=${limit}&safesearch=true`)
         .then(res => res.json())
         .then(data => setPictures(data.hits))
 
@@ -17,12 +31,24 @@ function App() {
   }
 
   useEffect(() => {
-    fetchPicture()
-  }, [])
+    fetchPicture(search, limit)
+  }, [search, limit])
 
   return (
     <>
-      <Header />
+      <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
+        <Container>
+          <Navbar.Brand href="#home">pixBay</Navbar.Brand>
+          <div className={styles.searchImage}>
+            <Form.Control placeholder="Search..." className={styles.searchField} onChange={searchHandler} value={search} />
+            <Form.Select aria-label="Select Image" className={styles.selectValue} onChange={showPerPage} value={limit} >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </Form.Select>
+          </div>
+        </Container>
+      </Navbar>
       <ImageLists pictureList={pictures} />
     </>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,21 @@
-import React, { useState, useRef } from "react"
-
-import { Container, Navbar, InputGroup, Form } from "react-bootstrap"
+import React, { useState } from "react"
+import { Container, Navbar, Form } from "react-bootstrap"
 import styles from "./Header.module.css"
-export default function Header() {
-  const [input, setInput] = useState({
-    searchText: ''
-  })
+
+
+export default function Header(searchValue) {
+
+  const [search, setSearch] = useState("text")
+
+  const searchHandler = (e) => setSearch(e.target.value)
+
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
       <Container>
         <Navbar.Brand href="#home">pixBay</Navbar.Brand>
         <div className={styles.searchImage}>
-          <Form.Control placeholder="Search..." className={styles.searchField} onChange={(e) => setInput({ searchText: e.target.value })} />
+          <Form.Control placeholder="Search..." className={styles.searchField} onChange={searchHandler} value={search} />
           <Form.Select aria-label="Select Image" className={styles.selectValue} onChange={(e) => console.log(e.target.value)} >
             <option value="10">10</option>
             <option value="20">20</option>
