fb95121be815ba7bd11ee2ba5e4a8d420d4b53c3
SheikhRashed <devrashed06@gmail.com>
2022-08-12 13:59:04 +0600

    update: passes components props parents to child

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from "react"
-// import Header from "./Components/Header/Header";
-import { Container, Navbar, Form } from "react-bootstrap"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import ImageLists from "./Components/ImageLists/ImageLists";
-
-import styles from "./Components/Header/Header.module.css"
+import Header from "./Components/Header/Header";
 
 function App() {
+
   const [search, setSearch] = useState("")
   const [limit, setLimit] = useState(5)
   const [pictures, setPictures] = useState([])
-
-  console.log(search)
-
-  const searchHandler = (e) => setSearch(e.target.value)
-  const showPerPage = (e) => setLimit(e.target.value)
-
-
 
   async function fetchPicture(search, limit) {
     try {
@@ -36,19 +26,7 @@ function App() {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
-        <Container>
-          <Navbar.Brand href="#home">pixBay</Navbar.Brand>
-          <div className={styles.searchImage}>
-            <Form.Control placeholder="Search..." className={styles.searchField} onChange={searchHandler} value={search} />
-            <Form.Select aria-label="Select Image" className={styles.selectValue} onChange={showPerPage} value={limit} >
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-            </Form.Select>
-          </div>
-        </Container>
-      </Navbar>
+      <Header search={search} setSearch={setSearch} limit={limit} setLimit={setLimit} />
       <ImageLists pictureList={pictures} />
     </>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,23 @@
-import React, { useState } from "react"
+import React from "react"
 import { Container, Navbar, Form } from "react-bootstrap"
 import styles from "./Header.module.css"
 
 
-export default function Header(searchValue) {
+export default function Header({ search, setSearch, limit, setLimit }) {
 
-  const [search, setSearch] = useState("text")
 
   const searchHandler = (e) => setSearch(e.target.value)
-
-
+  const showPerPage = (e) => setLimit(e.target.value)
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
       <Container>
         <Navbar.Brand href="#home">pixBay</Navbar.Brand>
         <div className={styles.searchImage}>
           <Form.Control placeholder="Search..." className={styles.searchField} onChange={searchHandler} value={search} />
-          <Form.Select aria-label="Select Image" className={styles.selectValue} onChange={(e) => console.log(e.target.value)} >
+          <Form.Select aria-label="Select Image" className={styles.selectValue} onChange={showPerPage} value={limit} >
+            <option value="5">5</option>
             <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">50</option>
+            <option value="15">15</option>
           </Form.Select>
         </div>
       </Container>
